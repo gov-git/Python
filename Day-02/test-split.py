7ff7dd13b91fc613a020537fcb717967a098f63c
gov-git <93032588+gov-git@users.noreply.github.com>
2023-12-27 11:33:40 +0000

    Updated some files accordingly.

--- Day-02/test-split.py
+++ Day-02/test-split.py
@@ -1,3 +1,16 @@
+# Split function for filtering
 arn = "arn:aws:iam:123456789012:user/johndoe"
 
 print(arn.split("/")[1])
+
+# Upper or Lower case conversion
+name = "Govardhan"
+print(name.upper())
+print(name.lower())
+
+# Concatination:
+str1 = "Hellow"
+str2 = "World"
+result = str1 + " " + str2
+print(result)
+
